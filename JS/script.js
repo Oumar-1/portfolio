0ff62c6e8f71e16cfc5dd10573e965d682fa8d9c
HITO <oumar2000mouhamed@gmail.com>
2022-08-21 00:10:51 +0300

    make header  diesapear after 3seconds if user unfocus from it in wide screen

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,26 @@
 const menu = document.querySelector(".menu");
 const navLinks = document.querySelector(".nav-links");
 const header = document.querySelector("header");
+
+let headerCountDown = setTimeout(() => {})
+
 menu.addEventListener("click", () => {
   navLinks.classList.toggle("hide");
+  if (window.innerWidth > 768) header.classList.toggle("hide");
+  
 });
+menu.addEventListener("mouseenter", () => {
+  if (window.innerWidth > 768) {
+    header.classList.remove("hide");
+    clearTimeout(headerCountDown);
+
+  }
+});
+menu.addEventListener("mouseleave", () => {
+  headerCountDown = setTimeout(() => {
+    header.classList.add("hide");
+  },3000)
+})
 function changeImg() {
   const img = document.querySelector(".menu img");
   let src1 = "../imgs/icons/menu.svg";
