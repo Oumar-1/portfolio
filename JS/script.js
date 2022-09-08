12156db550cae8f9954c63c9a50f397000a858bd
HITO <oumar2000mouhamed@gmail.com>
2022-09-08 21:51:31 +0300

    remove my own particles because of bad performance i have to learn more about canvas to realy do it on my own | just add light wait particles.js particles liberary

--- JS/script.js
+++ JS/script.js
@@ -1,11 +1,6 @@
-// landing canvas global variables
-
-const landingCanvas = document.querySelector("#landing-canvas"),
-  landingCtx = landingCanvas.getContext("2d"),
-  landingCanvasParent = landingCanvas.parentElement;
-let particlesArray = [];
-
-scalableCanvas(landingCanvas, landingCanvasParent);
+particlesJS.load('particles-js', 'Json/particles.json', function() {
+  console.log('callback - particles.js config loaded');
+});
 class Navbar {
   constructor() {
     this.navLinks = document.querySelectorAll(".nav-links li a");
@@ -62,7 +57,6 @@ class Navbar {
       });
     });
   }
-
   updateOnResize() {
     if (window.innerWidth > 768) {
       this.menuImg.src = this.menuImgSrc[1];
@@ -73,7 +67,6 @@ class Navbar {
     }
   }
 }
-
 class Landing {
   constructor() {
     this.section = document.querySelector(".landing");
@@ -88,182 +81,6 @@ class Landing {
     userAge.textContent = birthDate;
   }
 }
-
-function scalableCanvas(canvas, parent) {
-  // make any canvas scalable
-  canvas.width = parent.offsetWidth;
-  canvas.height = parent.offsetHeight;
-  window.addEventListener("resize", () => {
-    canvas.width = parent.offsetWidth;
-    canvas.height = parent.offsetHeight;
-  });
-}
-class Particle {
-  constructor(canvas, ctx, x, y, dirX, dirY, color, size, speed) {
-    this.canvas = canvas;
-    this.ctx = ctx;
-    this.x = x;
-    this.y = y;
-    this.dirX = dirX;
-    this.dirY = dirY;
-    this.speed = speed;
-    this.color = color ?? this.randomColor();
-    this.size = size;
-  }
-  randomColor() {
-    const colors = ["#32b9cd", "#2894a4", "#1e6f7b", "#144a52", "#0f373e"];
-    return colors[Math.floor(Math.random() * colors.length)];
-  }
-  draw() {
-    this.ctx.beginPath();
-    this.ctx.fillStyle = this.color;
-    this.ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2, false);
-    this.ctx.fill();
-  }
-}
-
-function createParticles() {
-  // options for ease modifying
-  const ballOptions = {
-    color: "#32b9cd",
-    /*put undefined to genearate random color from the array of colors in the particle prototype*/
-    minSpeed: 1,
-    maxSpeed: 1,
-    size: () => Math.random() * 0.5 + 1.5,
-    amount: 100,
-    bounce: true,
-    wait: false,
-  };
-  const connectOptions = {
-    color: "#1e6f7b",
-    width: 1,
-    connect: true,
-    connectDistance: 80,
-  };
-
-  // generate randome number between two nums
-  let randomNum = (min, max) => Math.random() * max + min;
-  initialParticles(ballOptions.amount);
-  // create particles loop
-  function initialParticles(amount) {
-    particlesArray = [];
-
-    let i = 1;
-    while (i <= amount) {
-      // i put this condition here instead of outside to give each particle unique position
-      let x,
-        y,
-        dirX,
-        dirY,
-        size = ballOptions.size();
-
-      if (ballOptions.bounce) {
-        x = Math.random() * landingCanvas.width;
-        y = Math.random() * landingCanvas.height;
-        dirX = Math.random() * 1 + -1;
-        dirY = Math.random() * 1 + -1;
-      } else {
-        x = Math.random() * landingCanvas.width;
-        y = landingCanvas.height + Math.random() * 500;
-        dirX = undefined;
-        dirY = 1;
-      }
-      // create the particles
-      particlesArray.push(
-        new Particle(
-          landingCanvas,
-          landingCtx,
-          x,
-          y + size * 2,
-          dirX,
-          dirY,
-          ballOptions.color,
-          ballOptions.size(),
-          randomNum(ballOptions.minSpeed, ballOptions.maxSpeed)
-        )
-      );
-      i++;
-    }
-  }
-
-  // initial update method
-  if (ballOptions.bounce) {
-    Particle.prototype.update = function () {
-      if (
-        this.x + this.size >= this.canvas.height ||
-        this.x - this.size * 2 <= 0
-      ) {
-        this.dirX *= -1;
-        this.speed = randomNum(ballOptions.minSpeed, ballOptions.maxSpeed);
-      }
-      if (
-        this.y + this.size >= this.canvas.height ||
-        this.y - this.size * 2 <= 0
-      ) {
-        this.dirY *= -1;
-        this.speed = randomNum(ballOptions.minSpeed, ballOptions.maxSpeed);
-      }
-      this.x += this.speed * this.dirX;
-      this.y += this.speed * this.dirY;
-    };
-  } else {
-    Particle.prototype.update = function () {
-      if (this.y + this.size <= 0) {
-        this.y = this.canvas.height + this.size;
-        this.x = Math.random() * this.canvas.width;
-        randomNum(ballOptions.minSpeed, ballOptions.maxSpeed);
-      }
-      this.y -= this.speed;
-    };
-  }
-
-  function animation() {
-    if (ballOptions.wait) {
-      setTimeout(() => {
-        window.requestAnimationFrame(animation);
-      }, 3000);
-      ballOptions.wait = false;
-      particlesArray.forEach((particle) => {
-        particle.draw();
-      });
-      return;
-    }
-    // landingCtx.clearRect(0, 0, landingCanvas.width, landingCanvas.height);
-
-    landingCtx.beginPath();
-    landingCtx.rect(0, 0, landingCanvas.width, landingCanvas.height);
-    landingCtx.fillStyle = "rgba(0,0,0,0.15)";
-    landingCtx.fill();
-
-    particlesArray.forEach((particle) => {
-      particle.update();
-      particle.draw();
-    });
-
-    window.requestAnimationFrame(animation);
-  }
-  animation();
-
-  // connect particles methode
-  function connectParticles() {
-    particlesArray.forEach((ball) => {
-      particlesArray.forEach((sibling) => {
-        let dx = Math.pow(ball.x - sibling.x, 2);
-        let dy = Math.pow(ball.y - sibling.y, 2);
-        let d = Math.sqrt(dx + dy);
-        if (d <= connectOptions.connectDistance) {
-          landingCtx.beginPath();
-          landingCtx.moveTo(ball.x, ball.y);
-          landingCtx.lineWidth = connectOptions.width;
-          landingCtx.strokeStyle = connectOptions.color;
-          landingCtx.lineTo(sibling.x, sibling.y);
-          landingCtx.stroke();
-        }
-      });
-    });
-  }
-}
-createParticles();
 
 const appNavbar = new Navbar();
 const landingPage = new Landing();
