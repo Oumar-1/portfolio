e7b5b08aadcbfb256b56a010044a1d23798e9b8f
HITO <oumar2000mouhamed@gmail.com>
2022-12-22 04:20:57 +0300

    remove the ugly and bad organized class and replace it with butifull functions and improve user experience by adding event listener for links when they clicked close the navigation bar and a lot more better

--- JS/script.js
+++ JS/script.js
@@ -1,89 +1,50 @@
 particlesJS.load("particles-js", "Json/particles.json");
-class Navbar {
-  constructor() {
-    this.navLinks = document.querySelectorAll(".nav-links li a");
-    this.navbar = document.querySelector(".navbar");
-    this.nav = document.querySelector(".nav-links");
-    this.menu = document.querySelector(".menu");
-    this.menuImg = document.querySelector(".menu img");
-    this.menuImgSrc = [
-      "../imgs/icons/nav/mob-menu.svg",
-      "../imgs/icons/nav/monitor-menu.svg",
-    ];
-    this.countDown = setTimeout(() => {
-      this.navbar.classList.remove("active");
-    }, 4000);
-    this.initial();
-  }
-  initial() {
-    this.navbar.addEventListener("mouseenter", () => this.handleMouseEnter());
-    this.navbar.addEventListener("mouseleave", () => this.handleMouseLeave());
-    this.menu.addEventListener("click", () => this.handleMenuClick());
-    this.updateOnResize();
-    this.handleLinksClick();
-    
+
+function navbar() {
+  const linksContainer = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-link");
+  const navbar = document.querySelector(".navbar");
+  const menu = document.querySelector(".menu");
+  const menuIcons = ["", ""];
+  let countDown;
+  navLinks.forEach((link) =>
+    link.addEventListener("click", () => {
+      navbar.classList.remove("active");
+    })
+  );
+  function handleMouseEnter() {
+    if (window.innerWidth < 768) return;
+    clearTimeout(countDown);
+    navbar.classList.add("active");
   }
 
-  handleMouseEnter() {
+  function handleMouseLeave() {
     if (window.innerWidth < 768) return;
-    clearTimeout(this.countDown);
-    this.navbar.classList.add("active");
-  }
-
-  handleMouseLeave() {
-    if (window.innerWidth < 768) return;
-    this.countDown = setTimeout(() => {
-      this.navbar.classList.remove("active");
+    countDown = setTimeout(() => {
+      navbar.classList.remove("active");
     }, 2000);
   }
 
-  handleMenuClick() {
-    if (window.innerWidth < 768) {
-      this.nav.classList.toggle("active");
-    } else {
-      this.navbar.classList.toggle("active");
-    }
+  function handleMenuClick() {
+    navbar.classList.toggle("active");
   }
 
-  handleLinksClick() {
-    this.navLinks.forEach((link) => {
-      link.addEventListener("click", () => {
-        if (window.innerWidth > 768) {
-          this.navbar.classList.remove("active");
-          return;
-        }
-        this.nav.classList.remove("active");
-      });
-    });
-  }
-  updateOnResize() {
+  function handleResize() {
     if (window.innerWidth > 768) {
-      this.menuImg.src = this.menuImgSrc[1];
-      this.navbar.classList.add("container");
+      menu.firstElementChild.src = "../imgs/icons/nav/monitor-menu.svg";
+      navbar.classList.add("container");
     } else {
-      this.menuImg.src = this.menuImgSrc[0];
-      this.navbar.classList.remove("container");
+      menu.firstElementChild.src = "../imgs/icons/nav/mob-menu.svg";
+      navbar.classList.remove("container");
     }
   }
-}
-class Landing {
-  constructor() {
-    this.section = document.querySelector(".landing");
-  }
-  printUserAge() {
-    const userAge = document.querySelector("[data-my-age]");
-    let birthDate = new Date("2000-07-12:00:00:00").getTime();
-    birthDate = Math.round(
-      (Date.now() - birthDate) / 1000 / 60 / 60 / 24 / 365
-    );
-    userAge.textContent = birthDate;
-  }
+
+  window.addEventListener("resize", handleResize);
+  if (window.innerWidth > 768) navbar.classList.add("active");
+  navbar.addEventListener("mouseenter", handleMouseEnter);
+  navbar.addEventListener("mouseleave", handleMouseLeave);
+  menu.addEventListener("click", handleMenuClick);
+  handleResize();
 }
 
-const appNavbar = new Navbar();
-const landingPage = new Landing();
-if (window.innerWidth > 768) appNavbar.navbar.classList.add("active");
-// Window Events
-window.addEventListener("resize", () => {
-  appNavbar.updateOnResize();
-});
+navbar();
