8353beece73d3d6466554be3637115c1f8a1c9ce
HITO <oumar2000mouhamed@gmail.com>
2022-12-25 03:13:38 +0300

    code enhancement

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,11 @@
 particlesJS.load("particles-js", "Json/particles.json");
+const smallScreens = 767;
+const largeScreens = 1200;
 
+function largeScreenCheck() {
+  if (window.innerWidth > smallScreens) return true;
+  return false;
+}
 function navbar() {
   const linksContainer = document.querySelector(".nav-links");
   const navLinks = document.querySelectorAll(".nav-link");
@@ -12,20 +18,21 @@ function navbar() {
       navbar.classList.remove("active");
     })
   );
-  function handleMouseEnter() {
-    if (window.innerWidth < 768) return;
-    clearTimeout(countDown);
-    navbar.classList.add("active");
-  }
+  const handleMouseEnter = () => {
+    if (largeScreenCheck()) {
+      clearTimeout(countDown);
+      navbar.classList.add("active");
+    }
+  };
+  const handleMouseLeave = () => {
+    if (largeScreenCheck()) {
+      countDown = setTimeout(() => {
+        navbar.classList.remove("active");
+      }, 2000);
+    }
+  };
 
-  function handleMouseLeave() {
-    if (window.innerWidth < 768) return;
-    countDown = setTimeout(() => {
-      navbar.classList.remove("active");
-    }, 2000);
-  }
-
-  function handleMenuClick() {
+  function toggleNav() {
     navbar.classList.toggle("active");
   }
 
@@ -43,7 +50,7 @@ function navbar() {
   if (window.innerWidth > 768) navbar.classList.add("active");
   navbar.addEventListener("mouseenter", handleMouseEnter);
   navbar.addEventListener("mouseleave", handleMouseLeave);
-  menu.addEventListener("click", handleMenuClick);
+  menu.addEventListener("click", toggleNav);
   handleResize();
 }
 
